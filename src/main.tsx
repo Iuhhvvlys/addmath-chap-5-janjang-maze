0d75a8eb8d4d564cd83fb9b87ef688ff731f0bf6
Iuhhvvlys <m-10995901@moe-dl.edu.my>
2025-08-06 23:52:39 +0800

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SiriMazeGame from './SiriMazeGame'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SiriMazeGame />
+  </React.StrictMode>,
+)
