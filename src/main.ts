acc82d56020b4112ea9507bcfb7c605f734b436d
Iuhhvvlys <m-10995901@moe-dl.edu.my>
2025-08-08 06:32:50 +0800

    Delete src directory

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-document.getElementById('app')!.innerHTML = `
-  <h1>🎮 Arithmetic Maze Game</h1>
-  <p>Click the correct number to go through the maze!</p>
-`;
--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SiriMazeGame from './SiriMazeGame'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <SiriMazeGame />
-  </React.StrictMode>,
-)
